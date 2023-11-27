47568f7a8e7f13d0e20e91fd056cedd867221edc
JohanneNissen <johanne@ornsovej.dk>
2023-11-27 11:29:22 +0100

    BuildingManagementSystem

--- src/main/java/BuildingManagementSystem.java
+++ src/main/java/BuildingManagementSystem.java
@@ -1,13 +1,18 @@
+import javax.xml.namespace.QName;
 import java.util.ArrayList;
 
 public class BuildingManagementSystem {
     private ArrayList<Building> buildingList = new ArrayList<Building>();
 
-    private void RemoveBuilding(){
-
+    private void RemoveBuilding(String name){
+        for(int i = 0; i < buildingList.size() ;i++){
+            if(name == buildingList(i).name){
+                buildingList.remove(i);
+            }
+        }
     }
 
     private void AddBuilding(){
-
+        buildingList.add(new Building());
     }
 }
